22dc79aa7f159c93b1c26fe3c73c85dc1456bb0c
bedvillain <amin8185@naver.com>
2026-07-25 18:33:23 +0900

    fix: InventoryManager 수정

--- Assets/03_Scripts/Managers/InventoryManager.cs
+++ Assets/03_Scripts/Managers/InventoryManager.cs
@@ -9,12 +9,12 @@ using UnityEngine;
 /// 담당:
 /// - 각 Player의 아이템 목록 관리
 /// - NetworkArray 기반 인벤토리 슬롯 관리
+/// - 아이템 보유 여부 확인
 /// - 아이템 추가 / 정렬
 /// - 사용 / 버리기 가능 여부 확인, 특정 아이템 보유 여부 확인
 /// 
 /// 참고:
 /// - Player 오브젝트에 붙는 것 기준
-/// - 
 /// </summary>
 public class InventoryManager : NetworkBehaviour
 {
@@ -22,8 +22,39 @@ public class InventoryManager : NetworkBehaviour
     // 비어있는 슬롯은 0으로 처리
     public const int EmptySlot = 0;
 
+    /// <summary>
+    /// 현재 씬에 존재하는 모든 플레이어 InventoryManager 목록
+    /// 팀 전체에 아이템 보유 여부를 확인할 때 사용
+    /// </summary>
+    public static readonly List<InventoryManager> AllInventories = new List<InventoryManager>();
+
     [Networked, Capacity(MaxSlotCount)]
     public NetworkArray<int> Slots => default;
+
+    [Networked] public int SelectedSlotIndex { get; set; }
+
+    /// <summary>
+    /// Fusion에서 Player NetworkObject가 Spawn된 후 호출
+    /// 전체 인벤토리 목록에 자신을 등록
+    /// </summary>
+    public override void Spawned()
+    {
+        if(!AllInventories.Contains(this))
+            AllInventories.Add(this);
+
+        if(Object.HasStateAuthority)
+            SelectedSlotIndex = 0;
+    }
+
+    /// <summary>
+    /// Fusion에서 Player NetworkObject가 Despawn될 때 호출
+    /// 전체 인벤토리 목록에서 자신을 제거
+    /// </summary>
+    public override void Despawned(NetworkRunner runner, bool hasState)
+    {
+        if (AllInventories.Contains(this))
+            AllInventories.Remove(this);
+    }
 
     /// <summary>
     /// 특정 itemId를 인벤토리에 가지고 있는지 확인
@@ -37,6 +68,19 @@ public class InventoryManager : NetworkBehaviour
                 return true;
         }
 
+        return false;
+    }
+
+    /// <summary>
+    /// 팀원 중 하나라도 itemId 가지고 있는지 확인
+    /// </summary>
+    public static bool HasAnyPlayerItem(int itemId)
+    {
+        foreach (InventoryManager inventory in AllInventories)
+        {
+            if (inventory != null && inventory.HasItem(itemId))
+                return true;
+        }
         return false;
     }
 
